dc4af4d25f5df27acd7e2a551500c0cda84ec0c3
mneoct <neo05988@stthomas.edu>
2020-03-15 01:23:25 -0500

    03152020
    Added class SkunkPlayerManagement:
    add and remove players.
    TODO: getter and setters for SkunkApp's variables.

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -6,122 +6,15 @@ import edu.princeton.cs.introcs.*;
 // Should negative chips (debt) be allowed???
 // allow removal of players at beginning (i.e. undo adding players...)
 // allow quitting in middle of game??
-// BP: rename items to follow conventions.
 // BP: resolve accessibilities after figuring out classes, methods.
 
-//set total chips to white,red,blue. Setup possible, but implementation is nightmare
-//nightmare to distribute chips to less than 8 people,
-// nightmare to manage the actual count of each type, how chips are exchanged:
-// e.g. give red chips, expect 2 white ones, what if they don't have any etc. 
-//private static final int blueChipsValue = 10;
-//private static final int redChipsValue = 5;
-//private static final int whiteChipsValue = 1;
-//private int blueChipsTotal = 16;
-//private int redChipsTotal = 32;
-//private int whiteChipsTotal = 80;
-
 public class SkunkApp { // main program
-	private static final int MAX_PLAYERS = 8;
+	public static final int MAX_PLAYERS = 8;
 	static final int OVERFLOWSCORE = 50; // = 100, set lower to test games faster...
 	private final static int TOTAL_CHIPS = 100; // = 400, set lower for test games faster...
 	public static Scanner myObj = new Scanner(System.in);
-	private static SkunkPlayer playersArray[];
+	private static SkunkPlayer[] playersArray;
 	
-	// Move to skunkPlayer.java
-	// addPlayerToArray() based on https://www.geeksforgeeks.org/how-to-add-an-element-to-an-array-in-java/
-	private static void addPlayers(){	// long, but kind of tricky to break..?
-		while (true) {
-			StdOut.println("Adding New Player...");
-			StdOut.println("Enter ... to continue without adding");
-			StdOut.println("Enter the new player's name:");
-			String userName = myObj.nextLine();
-			StdOut.println();
-			
-			if (userName.equals("...")) {
-				if (playersArray == null || playersArray.length < 2) {
-					StdOut.println("Insufficient players.");
-					continue;
-				}
-				else {
-					StdOut.println("Players have been registered.");
-					break;
-				}
-			}
-			else {
-				try { // players_array will be null at first, so exception on 1st run.
-					SkunkPlayer newPlayer = new SkunkPlayer(userName);
-					playersArray = addPlayerToArray(playersArray.length, playersArray, newPlayer);
-				}
-				catch (NullPointerException e) {
-					playersArray = new SkunkPlayer[1];
-					playersArray[0] = new SkunkPlayer(userName);
-				}
-			}
-			
-			try {
-				StdOut.println("Current number of players: " + playersArray.length);
-				for (SkunkPlayer player:playersArray) {
-					StdOut.println(player.getName());
-				}
-			}
-			catch (NullPointerException e) {
-				StdOut.println("Current number of players: " + playersArray.length);
-				for (SkunkPlayer player:playersArray) {
-					StdOut.println(player);
-				}
-			}
-			if (playersArray.length >= MAX_PLAYERS) {
-				StdOut.println("Max Players (" + MAX_PLAYERS + ") has been reached.");
-				break;
-			}
-			StdOut.println();
-		}
-		StdOut.println("Players Registration Complete.");
-		StdOut.println("Proceeding to gameplay");
-	}
-	public static SkunkPlayer[] addPlayerToArray(int currentPlayersCount,
-				SkunkPlayer[] CurrentSkunkPlayerArray, SkunkPlayer newPlayerToBeAdded){
-        int i; 
-        SkunkPlayer newSkunkArray[] = new SkunkPlayer[currentPlayersCount + 1]; 
-        for (i = 0; i < currentPlayersCount; i++) 
-            newSkunkArray[i] = CurrentSkunkPlayerArray[i]; 
-        newSkunkArray[currentPlayersCount] = newPlayerToBeAdded; 
-        return newSkunkArray; 
-    }
-	public static SkunkPlayer[] removePlayerFromArray(int currentPlayersCount,
-			SkunkPlayer[] CurrentSkunkPlayerArray, int playerRemovedIndex){
-	    int i; 
-	    int j = 0;
-	    SkunkPlayer newSkunkArray[] = new SkunkPlayer[currentPlayersCount - 1]; 
-	    for (i = 0; i < currentPlayersCount; i++)
-	    	if (i != playerRemovedIndex) {
-	    		newSkunkArray[j] = CurrentSkunkPlayerArray[i]; 
-	    		j++;
-	    	}
-	    return newSkunkArray; 
-	}
-	private static SkunkPlayer[] removePlayers(SkunkPlayer[] arrayOfPlayers) {
-		StdOut.println("Now checking for eliminated players...");
-		StdOut.println();
-		outerloop:
-		while (true) {
-			innerloop:
-			for (int i = 0; i < arrayOfPlayers.length + 1; i++) {
-				if (i >= arrayOfPlayers.length) {
-					StdOut.println("Done.");
-					break outerloop;
-				}
-				if (arrayOfPlayers[i].getPlayerChipsTotal() <= 0) {
-					StdOut.println("Removing " + arrayOfPlayers[i].getName());
-					arrayOfPlayers = removePlayerFromArray(arrayOfPlayers.length, arrayOfPlayers, i);			
-					StdOut.println();
-					break innerloop;
-				}
-			}
-		}
-		StdOut.println("Confirmation");
-		return arrayOfPlayers;
-	}
 	public static void distributeChips(SkunkPlayer[] inputPlayersArray) {
 		StdOut.println("Distribution of chips to players initiated...");
 		int chipDistributed = TOTAL_CHIPS / inputPlayersArray.length;
@@ -150,7 +43,7 @@ public class SkunkApp { // main program
 	}			
 	public static void startUpTournament() {
 		StdOut.println("Tournament has began...");
-		addPlayers();
+		playersArray = SkunkPlayerManagement.addPlayers();
 		StdOut.println();
 	}
 	private static void startUpGameResetValues() {
@@ -175,7 +68,7 @@ public class SkunkApp { // main program
 			SkunkGame.playGame(playersArray);
 			
 			StdOut.println("Post-Game Evaluation...");
-			playersArray = removePlayers(playersArray);
+			playersArray = SkunkPlayerManagement.removePlayers(playersArray);
 			printPlayersSheet();
 			
 			StdOut.println("Type 'end' to end the tournament, else it will continue");
---
+++ SkunkProject/src/SkunkPlayerManagement.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+import edu.princeton.cs.introcs.StdOut;
+
+public class SkunkPlayerManagement {
+	public static Scanner myObj = new Scanner(System.in);
+	private static int MAX_PLAYERS = SkunkApp.MAX_PLAYERS;
+	public static SkunkPlayer[] addPlayers(){	// long, but kind of tricky to break..?
+		SkunkPlayer[] playersArray = null;
+		while (true) {
+			StdOut.println("Adding New Player...");
+			StdOut.println("Enter ... to continue without adding");
+			StdOut.println("Enter the new player's name:");
+			String userName = myObj.nextLine();
+			StdOut.println();
+			
+			if (userName.equals("...")) {
+				if (playersArray == null || playersArray.length < 2) {
+					StdOut.println("Insufficient players.");
+					continue;
+				}
+				else {
+					StdOut.println("Players have been registered.");
+					break;
+				}
+			}
+			else {
+				try { // players_array will be null at first, so exception on 1st run.
+					SkunkPlayer newPlayer = new SkunkPlayer(userName);
+					playersArray = addPlayerToArray(playersArray.length, playersArray, newPlayer);
+				}
+				catch (NullPointerException e) {
+					playersArray = new SkunkPlayer[1];
+					playersArray[0] = new SkunkPlayer(userName);
+				}
+			}
+			
+			try {
+				StdOut.println("Current number of players: " + playersArray.length);
+				for (SkunkPlayer player:playersArray) {
+					StdOut.println(player.getName());
+				}
+			}
+			catch (NullPointerException e) {
+				StdOut.println("Current number of players: " + playersArray.length);
+				for (SkunkPlayer player:playersArray) {
+					StdOut.println(player);
+				}
+			}
+			if (playersArray.length >= MAX_PLAYERS) {
+				StdOut.println("Max Players (" + MAX_PLAYERS + ") has been reached.");
+				break;
+			}
+			StdOut.println();
+		}
+		StdOut.println("Players Registration Complete.");
+		StdOut.println("Proceeding to gameplay");
+		return playersArray;
+	}
+	private static SkunkPlayer[] addPlayerToArray(int currentPlayersCount,
+				SkunkPlayer[] CurrentSkunkPlayerArray, SkunkPlayer newPlayerToBeAdded){
+        int i; 
+        SkunkPlayer newSkunkArray[] = new SkunkPlayer[currentPlayersCount + 1]; 
+        for (i = 0; i < currentPlayersCount; i++) 
+            newSkunkArray[i] = CurrentSkunkPlayerArray[i]; 
+        newSkunkArray[currentPlayersCount] = newPlayerToBeAdded; 
+        return newSkunkArray; 
+    }
+	public static SkunkPlayer[] removePlayers(SkunkPlayer[] arrayOfPlayers) {
+		StdOut.println("Now checking for eliminated players...");
+		StdOut.println();
+		outerloop:
+		while (true) {
+			innerloop:
+			for (int i = 0; i < arrayOfPlayers.length + 1; i++) {
+				if (i >= arrayOfPlayers.length) {
+					StdOut.println("Done.");
+					break outerloop;
+				}
+				if (arrayOfPlayers[i].getPlayerChipsTotal() <= 0) {
+					StdOut.println("Removing " + arrayOfPlayers[i].getName());
+					arrayOfPlayers = removePlayerFromArray(arrayOfPlayers.length, arrayOfPlayers, i);			
+					StdOut.println();
+					break innerloop;
+				}
+			}
+		}
+		StdOut.println("Confirmation");
+		return arrayOfPlayers;
+	}
+	private static SkunkPlayer[] removePlayerFromArray(int currentPlayersCount,
+			SkunkPlayer[] CurrentSkunkPlayerArray, int playerRemovedIndex){
+	    int i; 
+	    int j = 0;
+	    SkunkPlayer newSkunkArray[] = new SkunkPlayer[currentPlayersCount - 1]; 
+	    for (i = 0; i < currentPlayersCount; i++)
+	    	if (i != playerRemovedIndex) {
+	    		newSkunkArray[j] = CurrentSkunkPlayerArray[i]; 
+	    		j++;
+	    	}
+	    return newSkunkArray; 
+	}
+}
