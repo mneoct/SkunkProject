d3a17580aefe320a731ad1e8e1d77189cdc3bfb0
0800-Mervyn <neo05988@stthomas.edu>
2020-05-14 23:57:38 -0500

    Code MR Stuff
    ...

---
+++ SkunkProject/codemr/SkunkProject_TP1 2/html/html_report_skunkproject_tp1/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"SkunkProject_TP1 working set: {SkunkProject_TP1}", "Number of Highly Problematic Classes":0, "Number of Entities":24, "Number of Problematic Classes":0, "Class Lines of Code":593, "Number of External Packages":4, "Number of Packages":2, "Number of External Entities":8}
